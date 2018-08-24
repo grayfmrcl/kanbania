884ddab4c83113f5b10fe352b87525f99901e869
Grayfield <grayfieldmm@outlook.com>
2018-08-24 20:43:28 +0700

    setup vuetify and firebase

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,8 @@
 <template>
-  <div id="app">
+  <vue-app id="app">
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  </vue-app>
 </template>
 
 <script>
